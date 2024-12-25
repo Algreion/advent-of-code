09d25ab6065df8a5cc62d8a3c213d67f6cf27c77
Algreion <188793676+Algreion@users.noreply.github.com>
2024-12-25 12:13:16 +0100

    Day 25 solution - Merry Christmas!

--- day_25.py
+++ day_25.py
@@ -1,31 +1,23 @@
 def d25_parse(input: str) -> tuple:
-    keys, locks = dict(), dict()
-    h = -1
-    for item in input.split("\n\n"):
-        lock = False
-        heights = dict()
-        for i,line in enumerate(item.splitlines()):
-            if h == -1: h = len(item.splitlines())
-            if i == 0 and "#" in line: lock = True
-            if lock:
-                for j in range(len(line)):
-                    if line[j] != "#" and j not in heights: heights[j] = i-1
-            else:
-                for j in range(len(line)):
-                    if line[j] == "#" and j not in heights: heights[j] = h-1-i
+    keys, locks = {}, {}
+    input = [item.splitlines() for item in input.split("\n\n")]
+    h = len(input[0])-1
+    for item in input:
+        lock = "#" in item[0]
+        heights = {}
+        for i,line in enumerate(item):
+            for j, c in enumerate(line):
+                if lock and c != "#" and j not in heights: heights[j] = i-1
+                elif not lock and c == "#" and j not in heights: heights[j] = h-i
         heights = tuple(v for _,v in sorted(heights.items()))
-        if lock: locks[heights] = locks.get(heights, 0) + 1
-        else: keys[heights] = keys.get(heights, 0) + 1
+        group = locks if lock else keys
+        group[heights] = group.get(heights, 0) + 1
     return (keys, locks, h)
 
 def day25_solve(input: str) -> int: # Ans: 3663
     keys, locks, h = d25_parse(input)
     matches = 0
-    for k in keys:
-        for l in locks:
-            yep = True
-            for i in range(len(k)):
-                if k[i] + l[i] >= h-1: yep = False
-            if yep: 
-                matches += keys[k] * locks[l]
+    for k, k_i in keys.items():
+        for l, l_i in locks.items():
+            if all(k[i] + l[i] < h for i in range(len(k))): matches += k_i * l_i
     return matches
